aa0ecccdbd5c6679f8742611e17457c4e53633c8
Kobe Oley <kgo6331@rit.edu>
2020-11-09 02:54:44 -0500

    Home Page correctly laid out

--- src/App.js
+++ src/App.js
@@ -1,64 +1,91 @@
 import React from 'react';
-import { Link, Route, Switch} from "react-router-dom";
+import {Link, Route, Switch} from "react-router-dom";
 
 import Home from "./Home";
 
 import './w3.css';
 
 const Covid = () => (
-  <div>
-    <h2>Covid</h2>
-  </div>
+    <div>
+        <h2>Covid</h2>
+    </div>
 );
 
 const Search = () => (
-  <div>
-    <h2>Search</h2>
-  </div>
+    <div>
+        <h2>Search</h2>
+    </div>
 );
 
 const Categories = () => (
-  <div>
-    <h2>Categories</h2>
-  </div>
+    <div>
+        <h2>Categories</h2>
+    </div>
 );
 
 const Login = () => (
-  <div>
-    <h2>Login</h2>
-  </div>
+    <div>
+        <h2>Login</h2>
+    </div>
 );
 
 function App() {
-  return (
-    <div className="w3-container">
-      <header className="w3-bar w3-white">
-        <div className="w3-bar-item">
-          <Link to="/">Home</Link>
+    return (
+        <div>
+            <header className="w3-bar w3-large w3-white">
+                <Link className="w3-bar-item w3-button w3-mobile"
+                      to="/"
+                      style={{textDecoration: 'none', width: '20%'}}>
+                    Home
+                </Link>
+                <Link className="w3-bar-item w3-button w3-mobile"
+                      to="/covid"
+                      style={{textDecoration: 'none', width: '20%'}}>
+                    COVID-19 Information
+                </Link>
+                <Link className="w3-bar-item w3-button w3-mobile"
+                      to="/search"
+                      style={{textDecoration: 'none', width: '20%'}}>
+                    Search
+                </Link>
+                <div className="w3-dropdown-hover w3-mobile" style={{width: '20%'}}>
+                    <button className="w3-button" style={{width: '100%'}}>Categories</button>
+                    <div className="w3-dropdown-content w3-bar-block w3-card-4" style={{width: '20%'}}>
+                        <Link className="w3-bar-item w3-button w3-center"
+                              to="/search/retail">
+                            Retail
+                        </Link>
+                        <Link className="w3-bar-item w3-button w3-center"
+                              to="/search/food">
+                            Food
+                        </Link>
+                        <Link className="w3-bar-item w3-button w3-center"
+                              to="/search/salon">
+                            Salon
+                        </Link>
+                        <Link className="w3-bar-item w3-button w3-center"
+                              to="/search/other">
+                            Other
+                        </Link>
+                    </div>
+                </div>
+                <Link className="w3-bar-item w3-button w3-mobile"
+                      to="/login"
+                      style={{textDecoration: 'none', width: '20%'}}>
+                    Login/Sign-up
+                </Link>
+            </header>
+            <div className="w3-container">
+                <Switch>
+                    <Route exact path="/"><Home/></Route>
+                    <Route path="/covid"><Covid/></Route>
+                    <Route path="/search"><Search/></Route>
+                    <Route path="/categories"><Categories/></Route>
+                    <Route path="/login"><Login/></Route>
+                </Switch>
+            </div>
         </div>
-        <div className="w3-bar-item">
-          <Link to="/covid">COVID-19 Information</Link>
-        </div>
-        <div className="w3-bar-item">
-          <Link to="/search">Search</Link>
-        </div>
-        <div className="w3-bar-item">
-          <Link to="/categories">Categories</Link>
-        </div>
-        <div className="w3-bar-item">
-          <Link to="/login">Login/Sign-up</Link>
-        </div>
-      </header>
-
-      <Switch>
-        <Route exact path="/"><Home /></Route>
-        <Route path="/covid"><Covid /></Route>
-        <Route path="/search"><Search /></Route>
-        <Route path="/categories"><Categories /></Route>
-        <Route path="/login"><Login /></Route>
-      </Switch>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/Home.js
+++ src/Home.js
@@ -4,14 +4,21 @@ import "./w3.css";
 
 function Home() {
 	return (
-		<div className="w3-display-topmiddle">
-			<form className="w3-container">
-				<input className="w3-input w3-border" type="text" id="search" placeholder="Search for your favorite business"/>
-				<input className="w3-button 3-blue" type="submit" value="Search"/>
+		<div className="w3-row">
+			<div className='w3-third w3-container'>
+			</div>
+			<form className="w3-third w3-panel w3-center">
+				<input className="w3-input w3-border"
+					   type="text"
+					   id="search"
+					   placeholder="Search for your favorite business"/>
+				<input className="w3-button w3-border w3-green"
+					   type="submit"
+					   value="Search"/>
+				<label className=" w3-panel w3-text-red">
+					This is where the error should go
+				</label>
 			</form>
-			<label className="w3-text-red">
-				This is where the error should go
-			</label>
 		</div>
 	);
 }
