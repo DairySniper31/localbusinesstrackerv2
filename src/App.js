216243c0b7bb2d9c876da7836b284860d1ac6a8d
Kobe Oley <kgo6331@rit.edu>
2020-11-08 23:25:57 -0500

    routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
+import { Link, Route, Switch} from "react-router-dom";
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
 function App() {
   return (
     <div>
       <header>
         <h1>This is the beginning of the actual app. Lets hope if this works</h1>
       </header>
+      <Route exact path="/"><Home /> </Route>
     </div>
   );
 }
