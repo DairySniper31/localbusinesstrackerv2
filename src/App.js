272a0a846108caf8124ba50ee89451cf260c464e
Kobe Oley <kgo6331@rit.edu>
2020-11-08 23:54:31 -0500

    header done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link, Route, Switch} from "react-router-dom";
 
+import './w3.css';
+
 const Home = () => (
   <div>
     <h2>Home</h2>
@@ -9,11 +11,24 @@ const Home = () => (
 
 function App() {
   return (
-    <div>
-      <header>
-        <h1>This is the beginning of the actual app. Lets hope if this works</h1>
+    <div className="w3-container">
+      <header className="w3-bar w3-white">
+        <div className="w3-bar-item">
+          <Link to="/">Home</Link>
+        </div>
+        <div className="w3-bar-item">
+          <Link to="/covid">COVID-19 Information</Link>
+        </div>
+        <div className="w3-bar-item">
+          <Link to="/search">Search</Link>
+        </div>
+        <div className="w3-bar-item">
+          <Link to="/categories">Categories</Link>
+        </div>
+        <div className="w3-bar-item">
+          <Link to="/login">Login/Sign-up</Link>
+        </div>
       </header>
-      <Route exact path="/"><Home /> </Route>
     </div>
   );
 }
